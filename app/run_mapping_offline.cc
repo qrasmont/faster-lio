4be89b8f873d2190dc413a4a13a1518a226699e5
Quentin <qrasmont@gmail.com>
2025-01-29 22:17:05 +0100

    WIP Migrate offline execution

--- app/run_mapping_offline.cc
+++ app/run_mapping_offline.cc
@@ -3,10 +3,21 @@
 //
 
 #include <gflags/gflags.h>
-#include <rosbag2_cpp/readers/sequential_reader.hpp>
-#include <rosbag2_cpp/typesupport_helpers.hpp>
 #include <unistd.h>
 #include <csignal>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include <rosbag2_cpp/reader.hpp>
+#include <rosbag2_cpp/readers/sequential_reader.hpp>
+#include <rosbag2_storage/serialized_bag_message.hpp>
+#include <rosbag2_storage/storage_options.hpp>
+#include <rclcpp/serialization.hpp>
+#include <rclcpp/serialized_message.hpp>
+
+#include <livox_ros_driver2/msg/custom_msg.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 #include "laser_mapping.h"
 #include "utils.h"
@@ -18,9 +29,13 @@ DEFINE_string(bag_file, "/home/xiang/Data/dataset/fast_lio2/avia/2020-09-16-quic
 DEFINE_string(time_log_file, "./Log/time.log", "path to time log file");
 DEFINE_string(traj_log_file, "./Log/traj.txt", "path to traj log file");
 
+DEFINE_string(r, "", "");
+DEFINE_string(ros_args, "", "");
+DEFINE_string(params_file, "", "");
+
 void SigHandle(int sig) {
     faster_lio::options::FLAG_EXIT = true;
-    RCLCPP_WARN("catch sig %d", sig);
+    std::cout << "catch sig" << sig << std::endl;
 }
 
 int main(int argc, char **argv) {
@@ -46,51 +61,83 @@ int main(int argc, char **argv) {
 
     // just read the bag and send the data
     LOG(INFO) << "Opening rosbag, be patient";
-    rosbag2_cpp::StorageOptions storage_options;
-    storage_options.uri = FLAGS_bag_file;
-    storage_options.storage_id = "sqlite3";
 
-    rosbag2_cpp::ConverterOptions converter_options;
+    // Configure and open the ROS 2 bag using rosbag2_cpp
+    rosbag2_cpp::Reader reader(std::make_unique<rosbag2_cpp::readers::SequentialReader>());
+    rosbag2_storage::StorageOptions storage_options;
+    storage_options.uri = FLAGS_bag_file;   // path to your .db3 folder
+    storage_options.storage_id = "sqlite3"; // or whatever storage plugin you use
+
+    rosbag2_cpp::ConverterOptions converter_options{};
+    // "cdr" is the default serialization format in most ROS 2 distros
     converter_options.input_serialization_format = "cdr";
     converter_options.output_serialization_format = "cdr";
 
-    rosbag2_cpp::readers::SequentialReader reader;
     reader.open(storage_options, converter_options);
 
+    rclcpp::Serialization<livox_ros_driver2::msg::CustomMsg> livox_serial;
+    rclcpp::Serialization<sensor_msgs::msg::PointCloud2> pcl_serial;
+    rclcpp::Serialization<sensor_msgs::msg::Imu> imu_serial;
+
     LOG(INFO) << "Go!";
-    for (const rosbag::MessageInstance &m : rosbag::View(bag)) {
-        auto livox_msg = m.instantiate<livox_ros_driver::CustomMsg>();
-        if (livox_msg) {
+    while (reader.has_next() && !faster_lio::options::FLAG_EXIT) {
+        auto bag_message = reader.read_next();
+        const auto &topic_name = bag_message->topic_name;
+
+        rclcpp::SerializedMessage serialized_msg(*bag_message->serialized_data);
+
+        if (topic_name == "/livox/lidar") {
+            livox_ros_driver2::msg::CustomMsg livox_msg;
+            livox_serial.deserialize_message(&serialized_msg, &livox_msg);
+
             faster_lio::Timer::Evaluate(
-                [&laser_mapping, &livox_msg]() {
-                    laser_mapping->LivoxPCLCallBack(livox_msg);
-                    laser_mapping->Run();
+                [&]() {
+                laser_mapping->LivoxPCLCallBack(std::make_shared<livox_ros_driver2::msg::CustomMsg>(livox_msg));
+                laser_mapping->Run();
                 },
-                "Laser Mapping Single Run");
-            continue;
+                "Laser Mapping Single Run"
+            );
         }
-
-        auto point_cloud_msg = m.instantiate<sensor_msgs::PointCloud2>();
-        if (point_cloud_msg) {
-            faster_lio::Timer::Evaluate(
-                [&laser_mapping, &point_cloud_msg]() {
-                    laser_mapping->StandardPCLCallBack(point_cloud_msg);
-                    laser_mapping->Run();
-                },
-                "Laser Mapping Single Run");
-            continue;
-        }
-
-        auto imu_msg = m.instantiate<sensor_msgs::Imu>();
-        if (imu_msg) {
-            laser_mapping->IMUCallBack(imu_msg);
-            continue;
-        }
-
-        if (faster_lio::options::FLAG_EXIT) {
-            break;
+        else if (topic_name == "/imu/data") {
+            sensor_msgs::msg::Imu imu_msg;
+            imu_serial.deserialize_message(&serialized_msg, &imu_msg);
+            laser_mapping->IMUCallBack(std::make_shared<sensor_msgs::msg::Imu>(imu_msg));
         }
     }
+
+    /* for (const rosbag::MessageInstance &m : rosbag::View(bag)) { */
+    /*     auto livox_msg = m.instantiate<livox_ros_driver::CustomMsg>(); */
+    /*     if (livox_msg) { */
+    /*         faster_lio::Timer::Evaluate( */
+    /*             [&laser_mapping, &livox_msg]() { */
+    /*                 laser_mapping->LivoxPCLCallBack(livox_msg); */
+    /*                 laser_mapping->Run(); */
+    /*             }, */
+    /*             "Laser Mapping Single Run"); */
+    /*         continue; */
+    /*     } */
+
+    /*     auto point_cloud_msg = m.instantiate<sensor_msgs::PointCloud2>(); */
+    /*     if (point_cloud_msg) { */
+    /*         faster_lio::Timer::Evaluate( */
+    /*             [&laser_mapping, &point_cloud_msg]() { */
+    /*                 laser_mapping->StandardPCLCallBack(point_cloud_msg); */
+    /*                 laser_mapping->Run(); */
+    /*             }, */
+    /*             "Laser Mapping Single Run"); */
+    /*         continue; */
+    /*     } */
+
+    /*     auto imu_msg = m.instantiate<sensor_msgs::Imu>(); */
+    /*     if (imu_msg) { */
+    /*         laser_mapping->IMUCallBack(imu_msg); */
+    /*         continue; */
+    /*     } */
+
+    /*     if (faster_lio::options::FLAG_EXIT) { */
+    /*         break; */
+    /*     } */
+    /* } */
 
     LOG(INFO) << "finishing mapping";
     laser_mapping->Finish();
